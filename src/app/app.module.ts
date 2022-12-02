bbab4b164fbad764b2dcf5675933b7cb133ebad3
Sandro Ribeiro <sandro@codtec.com>
2022-12-02 00:06:00 -0300

    build docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,12 +26,12 @@ import { environment } from 'src/environments/environment';
     LoginFormModule,
     UnauthenticatedContentModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !environment.production,//!isDevMode(),
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js', {
+      // enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }) : []
   ],
   providers: [
     AuthService,
